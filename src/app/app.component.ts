be6229ca566594b9d08ba75d33d46b67b2ceb3f5
Andrea Harris <andrea_harris2@cable.comcast.com>
2021-06-23 19:00:32 -0400

    finished part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Satellite } from './satellite';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+  sourcelist: Satellite [];
+  constructor() {
+    this.sourcelist = [];
+    //this.displayList = [];
+  }
 }
+
+
+
+
