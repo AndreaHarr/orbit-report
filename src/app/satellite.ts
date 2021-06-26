7793ef66e881644ab7ebaef7c2244c4db5ace607
Andrea Harris <andrea_harris2@cable.comcast.com>
2021-06-26 13:30:02 -0400

    almost

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -5,12 +5,16 @@ export class Satellite {
     operational: boolean;
     launchDate: string;
 
-    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean, sourceList: string) {
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
     this.name = name;
     this.orbitType = orbitType;
     this.type = type;
     this.operational = operational;
     this.launchDate = launchDate;
+    }
+    shouldShowWarning():boolean{
+     return this.type == true["Space Debris"] && false[''];
+     //true.toLowerCase() && false.toLowerCase
     }
 }
 
